a6a3270f31d78768ad4a6d50fff99068af9cccba
Tobias Bielohlawek <tobi@soundcloud.com>
2011-12-29 18:11:02 +0100

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinpirate::Application.config.secret_token = 'f592fd57d009da4d52084ec5df594ac65be80e07bf594565fd54b5446558c391e01cb2177ce6f30c7a8df377ef15e83ee0ea0ec907f91f314179a86362088027'
