4ac3b2345a9506aaf7b1cfeca38aa0fa1e587291
Tobias Bielohlawek <tobi@soundcloud.com>
2011-12-30 16:31:41 +0100

    only update best in place when not active

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -15,7 +15,10 @@ var request = function(gamesPath){
   });
 },
 updateGame = function(game){
-  $(".player .name").html('<span class="best_in_place" id="best_in_place_player_1_name" data-url="/players/' + game.player.id + '" data-object="player" data-attribute="name" data-type="input">' + game.player.name + '</span>');
+  if( $(".player .name form").size() == 0 ) {
+    $(".player .name").html('<span class="best_in_place" id="best_in_place_player_1_name" data-url="/players/' + game.player.id + '" data-object="player" data-attribute="name" data-type="input">' + game.player.name + '</span>');
+    $('.best_in_place').best_in_place();
+  }
   $(".player .slot").html(game.slot);
   $(".player .score").html(game.high_score);
   $(".player .digits span").each(function(index) {
@@ -23,7 +26,6 @@ updateGame = function(game){
              d = game.high_score_s.substr(index, 1) || 0;
     $digit.attr('class', "d" + d);
   });
-  $('.best_in_place').best_in_place();
 },
 schedule = function(){
   $('#games:not(.scheduled)').each(function(){
