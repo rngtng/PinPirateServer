3cda0965def25e28f63e6fe7519a86c82e66b71c
Tobias Bielohlawek <tobi@soundcloud.com>
2011-12-30 22:07:08 +0100

    fixed order and score height

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
 
   def index
     @game  = Game.latest.first
-    @games = Game.limit(10).all
+    @games = Game.limit(30).order("high_score DESC").all
 
     if request.xhr?
       render :json => @game.to_json( :only => [:id, :high_score, :slot, :name], :include => [:player], :methods => [:high_score_s] )
