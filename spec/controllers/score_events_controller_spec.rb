f0bb6e0eac1eaf2da5f84a31b703e927ace0a659
Tobias Bielohlawek <tobi@soundcloud.com>
2011-12-31 19:04:13 +0100

    added more score specs

--- spec/controllers/score_events_controller_spec.rb
+++ spec/controllers/score_events_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ScoreEventsController do
 
-  let(:game) { Game.create }
+  let!(:game) { create(:game, :score => 200, :slot => 1) }
   let(:raw_data) { ["12", "2", "30"] }
 
   it "creates score event" do
@@ -14,6 +14,26 @@ describe ScoreEventsController do
   it "returns json" do
     post :create, :game_id => game.id, :event => { :raw_data => raw_data }
     response.body.should == assigns(:event).to_json(:only => [:id, :game_id], :methods => [:score])
+  end
+
+  context "with lower score" do
+    let!(:game) { create(:game, :score => 1000, :slot => 1) }
+
+    it "adds score event to current game" do
+      expect do
+        post :create, :game_id => game.id, :event => { :raw_data => raw_data }
+      end.to change { Game.count }.by(1)
+    end
+  end
+
+  context "with different slot" do
+    let!(:game) { create(:game, :score => 200, :slot => 2) }
+
+    it "adds score event to current game" do
+      expect do
+        post :create, :game_id => game.id, :event => { :raw_data => raw_data }
+      end.to change { Game.count }.by(1)
+    end
   end
 
 end
